8e1bec6dac6054044daa4e5428ce502078cff2bb
emirhancakirr <ecakir20@ku.edu.tr>
2025-09-05 16:01:55 +0300

    last commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,15 @@ values(10002,current_date(),'rabi');
 
 insert into user_details(id,date,name)
 values(10003,current_date(),'rave');
+
+insert into post(id,description,user_id)
+values(20001,'learn AWS',10001);
+
+insert into post(id,description,user_id)
+values(20002,'learn DevOps',10001);
+
+insert into post(id,description,user_id)
+values(20003,'learn AWS',10002);
+
+insert into post(id,description,user_id)
+values(20004,'learn DevOps',10002);
